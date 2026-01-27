d042412829a8d43b7a6d9ab18628b72e0c490b51
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2026-01-27 11:29:46 +0100

    Update 01_server_crawling.py

--- 01_server_crawling.py
+++ 01_server_crawling.py
@@ -9,9 +9,13 @@ SERVER_RESULTS_FPATH = Path("/data/shared_space/bombarely_lab/AnnotationBenchmar
 GAQET_prefix = "AnnotationQC_"
 
 
+FOLDER_TRANSLATE = {""}
+
+
 def main():
     for filename in SERVER_RESULTS_FPATH.rglob("GAQET.log.txt"):
-        print(filename)
+        species = filename.split("/")[5]
+        
 
 
 
