1380f7e4651ff742338254ef4bbd21b059d52c12
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2026-01-29 15:56:52 +0100

    Update 02_GeMoMa_analysis.py

--- 02_GeMoMa_analysis.py
+++ 02_GeMoMa_analysis.py
@@ -1,12 +1,37 @@
 import yaml
 
+from pathlib import Path
 from sys import argv
 
 from src.docs import SPECIES_BY_ANNOT
 
 PROTOCOL_GeMoMA_PROTOCOL = "protocol_GeMoMaPipeline.txt"
+CONTRIBUTION_LINE = "annotation (Reference annotation file (GFF or GTF), which contains gene models annotated in the reference genome"
 
-def get_species_annotated_with_helixer(yaml_fhand):
+
+def get_contributions(pipeline_fhand):
+    contributions = {}
+    for line in pipeline_fhand:
+        if CONTRIBUTION_LINE in line:
+            contribution = line.rstrip.split()[-1]
+            if "GenomicData" in contribution:
+                contribution = contribution.split("/")[0].replace("GenomicData_", "").replace("_", " ")
+            else:
+                contribution = SPECIES_BY_ANNOT[contribution.rstrip().split("/")[-1]]
+                contributions[contribution] = {"distance": 0}
+    return contributions
+
+
+def add_species_contribution(benchmarks):
+    for species, benchmark in benchmarks.items():
+        for method, metadata in benchmark.items():
+            contribution_path = Path(metadata["report"]).parents[1] / PROTOCOL_GeMoMA_PROTOCOL
+            contributions = get_contributions(open(contribution_path))
+            metadata.update(contributions)
+
+
+
+def get_helixer_benchmarks(yaml_fhand):
     helixer_annnots = {}
     for species, annot in yaml_fhand.items():
         for method, metadata in annot.items():
@@ -21,8 +46,9 @@ def get_species_annotated_with_helixer(yaml_fhand):
 
 def main():
     metadata = yaml.safe_load(open(argv[1], "r"))
-    helixer_annots = get_species_annotated_with_helixer(metadata)
-    print(helixer_annots)
+    helixer_benchmarks = get_helixer_benchmarks(metadata)
+    helixer_benchmarks = add_species_contribution(helixer_benchmarks)
+    print(helixer_benchmarks)
 
 
 if __name__ == "__main__":
