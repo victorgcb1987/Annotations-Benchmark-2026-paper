0e027da6d830cd877dd96da254cb25ced575ddeb
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2026-03-05 13:27:04 +0100

    Update GeMoMa_02_gffcompare.py

--- GeMoMa_02_gffcompare.py
+++ GeMoMa_02_gffcompare.py
@@ -24,8 +24,6 @@ CONTRIBUTION_LINE = "annotation (Reference annotation file (GFF or GTF), which c
 def add_gffcompare_results(benchmarks, outbase):
     for species_a, annotations in benchmarks.items():
         for species_b, annotation_features in annotations.items():
-            if annotation_features == "tax_classification":
-                continue
             name = "GeMoMa_{}-{}".format(species_a, species_b)
             results = run_gffcompare(outbase, SOURCE_ANNOTS_FOR_GEMOMA[species_a], Path(annotation_features["annot_file"]), name)
             
